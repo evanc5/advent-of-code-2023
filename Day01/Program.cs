c19f8196bd575c271aaec30666464dbacd68c83b
Evan C <evanc5@users.noreply.github.com>
2023-12-01 11:59:26 -0500

    finish part 1, start part 2

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,4 +1,6 @@
-﻿Part1();
+﻿using System.Text.RegularExpressions;
+
+Part1();
 Part2();
 
 static void Part1()
@@ -9,7 +11,11 @@ static void Part1()
     var result = 0;
     foreach (var line in input)
     {
+        var firstDigit = line.First(char.IsDigit);
+        var lastDigit = line.Last(char.IsDigit);
 
+        var calibrationValue = int.Parse($"{firstDigit}{lastDigit}");
+        result += calibrationValue;
     }
 
     var elapsedTime = System.Diagnostics.Stopwatch.GetElapsedTime(startTime);
@@ -23,12 +29,38 @@ static void Part2()
     var startTime = System.Diagnostics.Stopwatch.GetTimestamp();
 
     var result = 0;
+
+    var pattern = @"([1-9]|one|two|three|four|five|six|seven|eight|nine)";
+    var rx = new Regex(pattern, RegexOptions.Compiled | RegexOptions.IgnoreCase);
     foreach (var line in input)
     {
+        var matches = rx.Matches(line);
+        var firstDigit = ParseDigit(matches.First().Value);
+        var lastDigit = ParseDigit(matches.Last().Value);
 
+        var calibrationValue = int.Parse($"{firstDigit}{lastDigit}");
+        result += calibrationValue;
     }
 
     var elapsedTime = System.Diagnostics.Stopwatch.GetElapsedTime(startTime);
     Console.WriteLine($"Part 2: {result}");
     System.Diagnostics.Debug.WriteLine($"Part 2: {elapsedTime}");
+}
+
+static string ParseDigit(string digit)
+{
+    var result = digit switch
+    {
+        "one" => "1",
+        "two" => "2",
+        "three" => "3",
+        "four" => "4",
+        "five" => "5",
+        "six" => "6",
+        "seven" => "7",
+        "eight" => "8",
+        "nine" => "9",
+        _ => digit
+    };
+    return result;
 }
