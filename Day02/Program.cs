88e34d634795574e9cdff6af7278c02a138fcecf
Evan C <evanc5@users.noreply.github.com>
2023-12-02 02:13:13 -0500

    finish part 2

--- Day02/Program.cs
+++ Day02/Program.cs
@@ -52,10 +52,34 @@ static void Part2()
     var input = File.ReadAllLines(@".\input.txt");
     var startTime = System.Diagnostics.Stopwatch.GetTimestamp();
 
+    var emptyBag = new Dictionary<string, int>()
+    {
+        {"red", 0},
+        {"green", 0},
+        {"blue", 0}
+    };
     var result = 0;
     foreach (var line in input)
     {
-
+        var games = line.Split(':')[1].Split(';');
+        var currentMin = emptyBag.ToDictionary();
+        foreach (var game in games)
+        {
+            var currentBag = emptyBag.ToDictionary();
+            var moves = game.Trim().Split(',');
+            foreach (var move in moves)
+            {
+                var split = move.Trim().Split(' ');
+                var count = int.Parse(split[0]);
+                var color = split[1];
+                currentBag[color] += count;
+            }
+            currentMin["red"] = Math.Max(currentMin["red"], currentBag["red"]);
+            currentMin["green"] = Math.Max(currentMin["green"], currentBag["green"]);
+            currentMin["blue"] = Math.Max(currentMin["blue"], currentBag["blue"]);
+        }
+        var power = currentMin["red"] * currentMin["green"] * currentMin["blue"];
+        result += power;
     }
 
     var elapsedTime = System.Diagnostics.Stopwatch.GetElapsedTime(startTime);
