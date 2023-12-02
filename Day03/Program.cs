27b7e975c5b105e84cd87040d55a94e1e3abc082
Evan C <evanc5@users.noreply.github.com>
2023-12-02 12:19:11 -0500

    set up day 3

---
+++ Day03/Program.cs
@@ -0,0 +1,34 @@
+﻿Part1();
+Part2();
+
+static void Part1()
+{
+    var input = File.ReadAllLines(@".\input.txt");
+    var startTime = System.Diagnostics.Stopwatch.GetTimestamp();
+
+    var result = 0;
+    foreach (var line in input)
+    {
+
+    }
+
+    var elapsedTime = System.Diagnostics.Stopwatch.GetElapsedTime(startTime);
+    Console.WriteLine($"Part 1: {result}");
+    System.Diagnostics.Debug.WriteLine($"Part 1: {elapsedTime}");
+}
+
+static void Part2()
+{
+    var input = File.ReadAllLines(@".\input.txt");
+    var startTime = System.Diagnostics.Stopwatch.GetTimestamp();
+
+    var result = 0;
+    foreach (var line in input)
+    {
+
+    }
+
+    var elapsedTime = System.Diagnostics.Stopwatch.GetElapsedTime(startTime);
+    Console.WriteLine($"Part 2: {result}");
+    System.Diagnostics.Debug.WriteLine($"Part 2: {elapsedTime}");
+}
