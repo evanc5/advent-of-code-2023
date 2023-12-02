9f5211450c74eb66a0c21ad241c77fac71159709
Evan C <evanc5@users.noreply.github.com>
2023-12-02 01:57:30 -0500

    complete part 1

--- Day02/Program.cs
+++ Day02/Program.cs
@@ -6,10 +6,40 @@ static void Part1()
     var input = File.ReadAllLines(@".\input.txt");
     var startTime = System.Diagnostics.Stopwatch.GetTimestamp();
 
+    var cubeBag = new Dictionary<string, int>()
+    {
+        {"red", 12},
+        {"green", 13},
+        {"blue", 14}
+    };
     var result = 0;
+    var id = 1;
     foreach (var line in input)
     {
-
+        var games = line.Split(':')[1].Split(';');
+        var possible = true;
+        foreach (var game in games)
+        {
+            var currentBag = cubeBag.ToDictionary();
+            var moves = game.Trim().Split(',');
+            foreach (var move in moves)
+            {
+                var split = move.Trim().Split(' ');
+                var count = int.Parse(split[0]);
+                var color = split[1];
+                currentBag[color] -= count;
+            }
+            if (currentBag.Any(v => v.Value < 0))
+            {
+                possible = false;
+                break;
+            }
+        }
+        if (possible)
+        {
+            result += id;
+        }
+        id++;
     }
 
     var elapsedTime = System.Diagnostics.Stopwatch.GetElapsedTime(startTime);
@@ -32,3 +62,4 @@ static void Part2()
     Console.WriteLine($"Part 2: {result}");
     System.Diagnostics.Debug.WriteLine($"Part 2: {elapsedTime}");
 }
+
