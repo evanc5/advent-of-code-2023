f0bd11727e4b2ffbc5914107a8d9c4bb1e173663
Evan C <evanc5@users.noreply.github.com>
2023-12-04 13:26:32 -0500

    complete part 2

--- Day04/Program.cs
+++ Day04/Program.cs
@@ -31,12 +31,73 @@ static void Part2()
     var startTime = System.Diagnostics.Stopwatch.GetTimestamp();
 
     var result = 0;
+    var cards = new List<Card>();
+    int id = 1;
     foreach (var line in input)
     {
+        var split = line.Split(':')[1].Split('|');
+        var winning = split[0].Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(int.Parse).ToArray();
+        var numbers = split[1].Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(int.Parse).ToArray();
+        cards.Add(new Card(id, winning, numbers));
+        id++;
+    }
 
+    foreach (var card in cards)
+    {
+        for (int i = 0; i < card.MatchCount; i++)
+        {
+            var nextIndex = card.Id + i;
+            if (nextIndex < cards.Count)
+            {
+                var nextCard = cards[nextIndex];
+                nextCard.Copies += card.Copies;
+            }
+        }
+        result += card.Copies;
     }
 
     var elapsedTime = System.Diagnostics.Stopwatch.GetElapsedTime(startTime);
     Console.WriteLine($"Part 2: {result}");
     System.Diagnostics.Debug.WriteLine($"Part 2: {elapsedTime}");
+}
+
+record class Card(int Id, int[] WinningNumbers, int[] ActualNumbers)
+{
+    public int Copies { get; set; } = 1;
+
+    public IEnumerable<int> Matches
+    {
+        get
+        {
+            return Enumerable.Intersect(WinningNumbers, ActualNumbers);
+        }
+    }
+
+    public int MatchCount
+    {
+        get
+        {
+            return Matches.Count();
+        }
+    }
+
+    public bool IsWinner
+    {
+        get
+        {
+            return MatchCount > 0;
+        }
+    }
+
+    public int Score
+    {
+        get
+        {
+            if (IsWinner)
+            {
+                return (int)Math.Pow(2, MatchCount - 1);
+            }
+            return 0;
+        }
+    }
 }
