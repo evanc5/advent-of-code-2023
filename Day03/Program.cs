839c5229f1b1007f469af1029aab87fd7a0284e7
Evan C <evanc5@users.noreply.github.com>
2023-12-04 16:59:58 -0500

    continue part 1 by searching for symbols

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -1,4 +1,7 @@
-﻿Part1();
+﻿using System.Text.RegularExpressions;
+using Microsoft.VisualBasic;
+
+Part1();
 Part2();
 
 static void Part1()
@@ -7,11 +10,49 @@ static void Part1()
     var startTime = System.Diagnostics.Stopwatch.GetTimestamp();
 
     var result = 0;
-
+    var lineNumber = 0;
+    var pattern = @"[^0-9.]";
+    var rx = new Regex(pattern, RegexOptions.Compiled);
+    foreach (var line in input)
+    {
+        var matches = rx.Matches(line);
+        foreach (Match match in matches)
+        {
+            var index = match.Index;
+            result += SearchForNumbers(input, index, lineNumber);
+        }
+        lineNumber++;
+    }
 
     var elapsedTime = System.Diagnostics.Stopwatch.GetElapsedTime(startTime);
     Console.WriteLine($"Part 1: {result}");
     System.Diagnostics.Debug.WriteLine($"Part 1: {elapsedTime}");
+}
+
+static int SearchForNumbers(string[] input, int index, int lineNumber)
+{
+    var result = 0;
+
+    var left = index - 1;
+    var right = index + 1;
+    var up = lineNumber - 1;
+    var down = lineNumber + 1;
+
+    for (int x = left; x <= right; x++)
+    {
+        if (x < 0 || x > input[index].Length) continue;
+        for (int y = up; y <= down; y++)
+        {
+            if (y < 0 || y > input.Length) continue;
+            if (char.IsDigit(input[y][x]))
+            {
+                //right, so I have the position of a digit, need to find the entire number
+                //need to also make sure we're not double counting anything
+            }
+        }
+    }
+
+    return result;
 }
 
 static void Part2()
